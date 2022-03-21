1eabe61d635195e132e3135c37d89f8414751ceb
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2022-03-22 03:59:16 +0530

    class components

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,49 @@
+import { Component } from "react";
 import { useParams } from "react-router-dom";
 
-const Details = () => {
-  const { id } = useParams();
-  return <h2>{id}</h2>;
+class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+
+    this.setState({ loading: false, ...json.pets[0] });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading...</h2>;
+    }
+
+    const { name, animal, breed, city, state, description } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} – {breed} – {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+// wrapping Details (class component) inside
+// function component, in order to use Hooks
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
 };
 
-export default Details;
+export default WrappedDetails;
